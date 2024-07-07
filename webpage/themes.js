13c50966214f00b54f273db2cd5039025bb6d2e2
pwompus <dariusleong8@gmail.com>
2024-07-08 00:20:29 +1000

    theme fetching now works yayy

--- webpage/themes.js
+++ webpage/themes.js
@@ -1,28 +1,52 @@
-const addStyleLinks = async (data) => {
-	for (let i = 0; i < data.files.length; i++) {
-		var link = document.createElement("link")
-			link.rel = "stylesheet"
-			link.type = "text/css"
-			link.href = data.files[i].path
-		document.querySelector("head").appendChild(link)
+class Themes {
+	constructor() {
+		this.fetched = false
+		this.themeData = undefined
+	}
+
+	get isFetched() {
+		return this.fetched
+	}
+
+	async fetchThemes() {
+		this.themeData = await fetch("http://127.0.0.1:8080/themes.json")
+			.then((data) => data.json())
+			//.then(() => { this.fetched = true })
+		this.fetched = true
+	}
+
+	async addStyleLinks() {
+		for (let i = 0; i < this.themeData.files.length; i++) {
+			var link = document.createElement("link")
+				link.rel = "stylesheet"
+				link.type = "text/css"
+				link.href = this.themeData.files[i].path
+			document.querySelector("head").appendChild(link)
+		}
+		console.log(this.themeData)
+	}
+
+	async updateSelectionOptions() {
+		var options = []
+		for (let i = 0; i < this.themeData.files.length; i++) {
+			options.push(this.themeData.files[i].name)
+		}
+		return options
 	}
 }
+const themes = new Themes()
+themes.fetchThemes()
 
-const updateThemeSelectionOptions = async () => {
-	var options = []
+const themeAction = setInterval(() => {
+            console.log(themes.isFetched)
 
-	// Have to have this here, since we can't just make the fetch result a global variable smh
-	const themeData = await fetch("http://127.0.0.1:8080/themes.json")
-		.then((data) => data.json())
+            if (themes.isFetched) {
+                themes.addStyleLinks()
+                this.themeOptions = themes.updateSelectionOptions()
+                clearInterval(themeAction)
 
-	for (let i = 0; i < themeData.files.length; i++) {
-		options.push(themeData.files[i].name)
-	}
-	return options
-}
-
-window.onload = async () => {
-	const themeData = await fetch("http://127.0.0.1:8080/themes.json")
-		.then((data) => data.json())
-	addStyleLinks(themeData)
-}
+                console.log(this.themeData)
+                console.log("themeAction done")
+            }
+        }, 100)
+console.log("interval set")
